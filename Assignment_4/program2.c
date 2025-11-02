debdfdaa91f0b4e2d7b5e8e8c65f91952988ebd6
Rupesh Patil <rupeshpatil.contact@gmail.com>
2025-11-02 19:48:02 +0530

    Add files via upload

---
+++ Assignment_4/program2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void FactRev(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            printf("%d\t", iCnt);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    FactRev(iValue);
+
+    return 0;
+}
