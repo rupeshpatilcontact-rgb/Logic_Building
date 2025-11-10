296054c20e2077efa18042ba82efb5693bfa3cf7
Rupesh Patil <rupeshpatil.contact@gmail.com>
2025-11-10 09:32:18 +0530

    Add files via upload

---
+++ Assignment_7/program1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Patten(int iNo)
+{
+    int iCnt = 0;
+
+   for(iCnt = 0; iCnt < iNo; iCnt++)
+   {
+        printf("$\t*\t",iCnt);
+   }
+
+   printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number ");
+    scanf("%d",&iValue);
+
+    Patten(iValue);
+
+    return 0;
+}
